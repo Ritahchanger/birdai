093ce5a6a9bbdf0c8a144cf5fdae805f28cbef25
Dennis Peter <peterdennis573@gmail.com>
2024-09-13 11:32:28 +0300

    Updates

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,6 +1,18 @@
 import "./main.css";
 import { assets } from "../../assets/assets";
+import { useContext } from "react";
+import { Context } from "../../context/context";
 const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
   return (
     <div className="main">
       <div className="nav">
@@ -8,30 +20,55 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello, Dev</span>
-            <p>How can I help you today?</p>
-          </p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest beautiful places to see on an upcomming road trip</p>
-            <img src={assets.compass_icon} alt="" />
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Dev</span>
+                <p>How can I help you today?</p>
+              </p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcomming road trip</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Briefly summarize this concept: urban planning</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Brainstorm team bonding activities for our work retreat</p>
+                <img src={assets.menu_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Update readability of the following code</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+
+              <img src={ assets.user } alt="" />
+
+              <p>
+                {recentPrompt}
+              </p>
+
+              <div className="result-data">
+                <img src={assets.gemini_icon} alt="" />
+
+                <p dangerouslySetInnerHTML={ {__html:resultData } }>
+
+                </p>
+              </div>
+
+            </div>
           </div>
-          <div className="card">
-            <p>Briefly summarize this concept: urban planning</p>
-            <img src={assets.bulb_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Brainstorm team bonding activities for our work retreat</p>
-            <img src={assets.menu_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Update readability of the following code</p>
-            <img src={assets.code_icon} alt="" />
-          </div>
-        </div>
+        )}
+
         <div className="main-bottom">
           <div className="search-box">
             <input
@@ -39,19 +76,20 @@ const Main = () => {
               name=""
               id=""
               placeholder="Enter something to search.."
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
             />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img src={assets.send_icon} alt="" />
+              <img src={assets.send_icon} alt="" onClick={onSent} />
             </div>
           </div>
           <p className="bottom-info">
-          Gemini may display inaccurate info, including about people, so
-          double-check its responses. Your privacy & Gemini Apps
-        </p>
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses. Your privacy & Gemini Apps
+          </p>
         </div>
-       
       </div>
     </div>
   );
---
+++ src/config/Gemini.js
@@ -0,0 +1,54 @@
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  }  from "@google/generative-ai";
+const APIKey = "AIzaSyDIPOsBx-9DqOmwoCaP43b3TeWctY9otAo";
+
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+
+
+const apiKey = process.env.GEMINI_API_KEY || APIKey;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+ 
+  console.log(result.response.text());
+  const usersChart = result.response.text()
+
+  return usersChart
+
+}
+
+export default run;
