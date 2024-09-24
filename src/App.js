5215fc8a2a1707036f5237210202914c199b4ac1
Dennis Peter <peterdennis573@gmail.com>
2024-09-24 13:14:35 +0300

    Updates on the swahili updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Main from "./components/main/Main";
 import Sidebar from "./components/sidebar/Sidebar";
 import { useState } from "react";
 const App = () => {
-  const [selectedTopic, setSelectedTopic] = useState("swh_Latin");
+  const [selectedTopic, setSelectedTopic] = useState("swh_Latn");
   return (
     <>
       <Sidebar selectedTopic={selectedTopic} />
